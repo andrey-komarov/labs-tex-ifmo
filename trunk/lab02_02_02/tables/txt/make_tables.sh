bf31c97e3c4b7d570f6eeea7c97f646e7965dbad
krotkovpavel@gmail.com <krotkovpavel@gmail.com@fe0e9b8c-1696-3028-093f-6366d50a7e92>
2011-05-18 16:57:19 +0000

    ''

---
+++ trunk/lab02_02_02/tables/txt/make_tables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in *.txt
+do
+    cp $i txt2tex.in
+    java txt2tex
+    cp txt2tex.out ../$i
+done
+
+for i in ../*.txt; do j=`echo $i | sed 's/txt/tex/g'`; mv "$i" "$j"; done
