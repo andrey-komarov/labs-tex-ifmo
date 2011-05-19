dd8d34ba52c89c857c9e4b270c70283eab71719e
krotkovpavel <krotkovpavel@gmail.com>
2011-05-19 20:44:17 +0000

    ''

--- lab02_02_02/tables/txt/make_tables.sh
+++ lab02_02_02/tables/txt/make_tables.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 javac txt2tex.java
+javac txt2tex2.java
 
-for i in *.txt
-do
-    cp $i txt2tex.in
-    java txt2tex
-    cp txt2tex.out ../$i
-done
+cp table1.txt txt2tex.in
+java txt2tex2
+cp txt2tex.out ../table1.tex
+
+cp table2.txt txt2tex2.in
+java txt2tex
+cp txt2tex.out ../table2.tex
+
+
 
 for i in ../*.txt; do j=`echo $i | sed 's/txt/tex/g'`; mv "$i" "$j"; done
